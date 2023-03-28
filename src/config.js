e538034e413faac8eb978f8f54907a5f0a235726
uxin312 <uxin312@163.com>
2023-03-28 17:28:53 +0800

    在线获取分类导航

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-const SERVE = false; // 是否使用线上服务器
+const SERVE = true; // 是否使用线上服务器
 
-const HOST = "https://esunr.xyz:8080/api/esunrIndex"; // 线上服务器地址，如果不使用请留空
+const HOST = "http://api.inix.fun:1101"; // 线上服务器地址，如果不使用请留空
 
 const BLOG_NAME = "我的博客"; // 个人网站名字
 
